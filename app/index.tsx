ea2b4d691072039209f7bc22d2e1d892aa202d7c
ynr <ya_onur@hotmail.com>
2023-06-05 03:49:24 +0300

    tailwind expor router done

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,20 @@
-import { View, Text, Pressable } from 'react-native'
-import {Link, useRouter} from 'expo-router'
-import React from 'react'
+import { View, Text, Pressable } from "react-native";
+import { Link, useRouter } from "expo-router";
+import React from "react";
 
 const LoginPage = () => {
-	const router = useRouter()
-	const handlePress=()=>{
-
-		router.replace('app')
-	}
+  const router = useRouter();
+  const handlePress = () => {
+    router.replace("app");
+  };
   return (
-	<View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-		<Text>SOme fancy logo</Text>
-	  
-	
-	  <Text>FAncy slide bar to go register</Text>
-	  <Link href={'/register'}>Create Account</Link>
-	</View>
-  )
-}
+    <View className="flex-1 justify-center items-center ">
+      <Text>SOme fancy logo</Text>
 
-export default LoginPage
+      <Text>FAncy slide bar to go register</Text>
+      <Link href={"/register"}>Create Account</Link>
+    </View>
+  );
+};
+
+export default LoginPage;
